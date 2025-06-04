15c92540a6344761cb9e97600e8c248aea7836f7
Marvio Rocha <marviorocha@marviorocha.com>
2025-06-03 23:30:25 -0300

    feat: Send welcome email after user creation
    This commit introduces a welcome email notification sent to new users upon account creation.
    
    - Implemented an `after_create` callback in the `User` model to trigger the `send_welcome_email` method.
    - The `send_welcome_email` method utilizes the `UserMailer` to deliver a welcome email asynchronously using `deliver_later`.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
 
   scope :by_company, -> (identifier) { where(company_id: identifier) if identifier.present? }
   scope :by_username, -> (username) { where('username LIKE ?', "%#{username}%") if username.present? }
+
+  after_create :send_welcome_email
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome_email(self).deliver_later
+  end
 end
